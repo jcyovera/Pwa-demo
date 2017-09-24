bac75b5a6ff25c46884831a975af219fcc06eb50
Juan Carlos <jcyovera@gmail.com>
2017-09-23 23:53:21 -0500

    Fixed issues with images

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -7,47 +7,49 @@ import { Book } from '../models/book.model';
 @Injectable()
 export class BookService {
   booksApiUrl = 'https://www.googleapis.com/books/v1/volumes';
-  bestSellersApiUrl='https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json';
+  bestSellersApiUrl = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json';
   constructor(private http: Http) { }
 
   private replaceHttpsInProps(book: Book): Book {
-    const propsToModify = ['thumbnail', 'smallThumbnail'];
+    const propsToModify = ['thumbnail', 'smallThumbnail', 'medium'];
     propsToModify.forEach((prop) => {
       if (book.volumeInfo && book.volumeInfo.imageLinks) {
-        book.volumeInfo.imageLinks[prop] = book.volumeInfo.imageLinks[prop].replace('http://', 'https://');
+        if (book.volumeInfo.imageLinks[prop]) {
+          book.volumeInfo.imageLinks[prop] = book.volumeInfo.imageLinks[prop].replace('http://', 'https://');
+        }
       }
     })
     return book;
   }
   searchBooks(query: string): Observable<any> {
     return this.http.get(`${this.booksApiUrl}/?q=${query}`)
-          .map((res: any) => {
-            res = res.json();
-            return res.items.map(this.replaceHttpsInProps)
-          });
+      .map((res: any) => {
+        res = res.json();
+        return res.items.map(this.replaceHttpsInProps)
+      });
   }
 
   getBookById(id: string): Observable<any> {
     return this.http.get(`${this.booksApiUrl}/${id}`)
-          .map((resp) => {
-            const book = resp.json()
-            return this.replaceHttpsInProps(book);
-          });
+      .map((resp) => {
+        const book = resp.json()
+        return this.replaceHttpsInProps(book);
+      });
   }
-  getBestSellersFromNYT():Observable<any> {
+  getBestSellersFromNYT(): Observable<any> {
     let params: URLSearchParams = new URLSearchParams();
     params.set('api-key', '112cc281f201470b811aac69ed0cc809');
-    return this.http.get(`${this.bestSellersApiUrl}`,{search:params})
-          .map((resp) => {
-            const book = resp.json()
-            return this.replaceHttpsInProps(book);
-          });
+    return this.http.get(`${this.bestSellersApiUrl}`, { search: params })
+      .map((resp) => {
+        const book = resp.json()
+        return this.replaceHttpsInProps(book);
+      });
   }
   searchBestSellersByIsbn(isbn: string): Observable<any> {
     return this.http.get(`${this.booksApiUrl}/?q=''+isbn=${isbn}`)
-          .map((res: any) => {
-            res = res.json();
-            return res.items.map(this.replaceHttpsInProps)
-          });
+      .map((res: any) => {
+        res = res.json();
+        return res.items.map(this.replaceHttpsInProps)
+      });
   }
 }
